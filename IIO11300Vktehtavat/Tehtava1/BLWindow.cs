af36e1c7e9b62e60b16b22d0712b212f1aaffe75
Replica94 <santeritaponen@gmail.com>
2016-01-21 11:10:25 +0200

    Tuntihommia

--- IIO11300Vktehtavat/Tehtava1/BLWindow.cs
+++ IIO11300Vktehtavat/Tehtava1/BLWindow.cs
@@ -4,14 +4,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tehtava1
+namespace JAMK.IT.IIO11300
 {
     public class BusinessLogicWindow
     {
+        public class Ikkuna
+        {
+            #region Muuttujat (variables)
+            private double korkeus;
+            private double leveys;
+            private double pintaala;
+            #endregion
+            #region Ominaisuudet (properties)
+            //oliosuunnittelun aikana on päätetty että pinta-ala ja hinta ovat read-only ominaisuuksia
+            public double PintaAla
+            {
+                get
+                {
+                    return korkeus * leveys;
+                }
+            
+            }
+            public float Hinta
+            {
+                get
+                {
+                    return LaskeHinta();
+                }
+            }
+            public double Korkeus
+            {
+                get
+                {
+                    return korkeus;
+                }
+                set
+                {
+                    //tässä kohdassa voisi tarvittaessa tehdä tarkistuksia
+                    korkeus = value;
+                }
+            }
+            public double Leveys
+            {
+                get
+                {
+                    return leveys;
+                }
+                set
+                {
+                    leveys = value;
+                }
+            }
+            private float LaskeHinta()
+            {
+                //hinta lasketaan työn, materiaalimenekin ja katteen mukaan
+                float kate = 100;
+                float tyo = 200;
+                float materiaali;
+                materiaali = 100 * (float)(leveys * korkeus / 100000);
+                return (kate + tyo + materiaali);
+            }
+        
+            #endregion
+            #region Konstruktorit (constructors)
+            #endregion
+            #region Metodit (methods)
+
+            public double LaskePintaAla()
+            {
+                return korkeus * leveys;
+            }
+            #endregion
+        }
+        public class IkkunaVE0
+        {
+            //joskus tehdään näin "oikaistaan"
+            //ei suositella by Esa
+            public double korkeus;
+            public double leveys;
+            public double LaskePintaAla()
+            {
+                return korkeus * leveys;
+            }
+        }
         /// <summary>
         /// CalculatePerimeter calculates the perimeter of a window
         /// </summary>
-        public static double CalculatePerimeter(double widht, double height)
+        public static double CalculatePerimeter(double width, double height, double frame)
         {
             throw new System.NotImplementedException();//moi
         }
--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -19,7 +19,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Tehtava1
+
+namespace JAMK.IT.IIO11300
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
@@ -34,10 +35,21 @@ namespace Tehtava1
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
             //TODO
+            double width;
+            double height;
+            double frame;
+            double perimeter;
+            double windowarea;
+            double framearea;
+
             try
             {
-                double result;
-                result = BusinessLogicWindow.CalculatePerimeter(1, 1);
+                width = double.Parse(txtWidht.Text);
+                height = double.Parse(txtHeight.Text);
+                frame = double.Parse(txtFrameWidth.Text);
+                perimeter = BusinessLogicWindow.CalculatePerimeter(width, height, frame);
+               // double result;
+               // result = BusinessLogicWindow.CalculatePerimeter(1, 1);
                 //Ei näin: BusinessLogicWindow.CalculatePerimeter(1, 1);
             }
             catch (Exception ex)
@@ -54,7 +66,21 @@ namespace Tehtava1
     {
       Application.Current.Shutdown();// Sovellus(käynnissä oleva) suljetaan tällä komennolla
     }
-  }
+
+        private void btnCalculateOO_Click(object sender, RoutedEventArgs e)
+        {
+            //oliona avulla pita-ala, piiri ja hinta
+            //luodaan olio
+            BusinessLogicWindow.Ikkuna ikk = new BusinessLogicWindow.Ikkuna();
+            ikk.Korkeus = double.Parse(txtHeight.Text);
+            ikk.Leveys = double.Parse(txtWidht.Text);
+            //pinta-alan lakeminen kutsumalla metodia
+            txtWindowArea.Text = ikk.LaskePintaAla().ToString;
+            //pinta-ala on olion ominaisuus
+            txtWindowArea.Text = ikk.PintaAla.ToString;
+          
+        }
+    }
 
   
 }
