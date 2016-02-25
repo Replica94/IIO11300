6df5c16d2192b1d0f1a0d34bd47a7e1195182bde
Replica94 <santeritaponen@gmail.com>
2016-02-25 11:04:54 +0200

    H6Databinding

---
+++ IIO11300Vktehtavat/H6Databinding/Hockey.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6Databinding
+{
+    public class HockeyPlayer
+    {
+
+    }
+
+    public class HockeyTeam
+    {
+        #region PROPERTIES
+        //HUOM! public field ei kelpaa XAMLin bindauksessa, pitää olla property
+        public string Name { get; set; }
+        public string City { get; set; }
+        #endregion
+        #region CONSTRUCTORS
+        public HockeyTeam()
+        {
+            Name = "Default";
+            City = "None";
+        }
+        public HockeyTeam(string teamName, string city)
+        {
+            Name = teamName;
+            City = city;
+        }
+        #endregion
+        public override string ToString()
+        {
+            return Name + "@" + City;
+        }
+    }
+
+    public class HockeyLeague
+    {
+        //perustetaan SMLiiga
+        List<HockeyTeam> teams = new List<HockeyTeam>();
+        public HockeyLeague()
+        {
+            teams.Add(new HockeyTeam("Ilves", "Tampere"));
+            teams.Add(new HockeyTeam("JYP", "Jyväskylä"));
+            teams.Add(new HockeyTeam("HIFK", "Helsinki"));
+            teams.Add(new HockeyTeam("Kärpät", "Oulu"));
+            teams.Add(new HockeyTeam("Kalpa", "Kuopio"));
+        }
+        //Metodi joka palauttaa liigan joukkueet
+        public List<HockeyTeam> GetTeams()
+        {
+            return teams;
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/H6Databinding/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H6Databinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HockeyLeague smliiga;
+        List<HockeyTeam> liigajoukkueet;
+        int osoitin;
+        public MainWindow()
+        {
+            InitializeComponent();
+            AlustaKontrollit();
+        }
+        private void AlustaKontrollit()
+        {
+            //Täytetään combobox listan alkioilla
+            List<string> joukkueet = new List<string>();
+            joukkueet.Add("Ilves");
+            joukkueet.Add("JYP");
+            joukkueet.Add("Kärpät");
+            cbTeams.ItemsSource = joukkueet;
+            //perustetaan SMLiiga
+            smliiga = new HockeyLeague();
+            liigajoukkueet = smliiga.GetTeams();
+
+        }
+        private void btnGetSettings_Click(object sender, RoutedEventArgs e)
+        {
+            //koodi lukee App.Configin asetuksen UserName
+            btnGetSettings.Content = H6Databinding.Properties.Settings.Default.UserName;
+            
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            //sidotaan kokoelma stackpanelin datakontekstiksi
+            spLiiga.DataContext = liigajoukkueet;
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            //siirretään osoitinta kokoelmassa yhdellä eteenpäin
+            osoitin++;
+            spLiiga.DataContext = liigajoukkueet[osoitin];
+        }
+
+        private void btwBackward_Click(object sender, RoutedEventArgs e)
+        {
+            osoitin--;
+            spLiiga.DataContext = liigajoukkueet[osoitin];
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            //lisätään kokoelmaan uusi joukkue
+            HockeyTeam uusi = new HockeyTeam();
+            liigajoukkueet.Add(uusi);
+            osoitin = liigajoukkueet.Count - 1;
+            spLiiga.DataContext = liigajoukkueet[osoitin];
+        }
+    }
+}
